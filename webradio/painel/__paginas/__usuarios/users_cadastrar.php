b09547e6e2b23492f418c1c9468db70deefba157
Miller <millerp@outlook.com>
2013-01-28 02:39:50 -0200

    Formulario de cadastro de usuarios

--- webradio/painel/__paginas/__usuarios/users_cadastrar.php
+++ webradio/painel/__paginas/__usuarios/users_cadastrar.php
@@ -36,34 +36,75 @@
           </div>
         </div>
         <div class="mws-form-row">
-          <label>URL Validation</label>
+          <label>Nome:</label>
           <div class="mws-form-item large">
-            <input type="text" name="urlField" class="mws-textinput required url" />
+            <input type="text" name="nome" class="mws-textinput required" />
           </div>
         </div>
         <div class="mws-form-row">
-          <label>Digit Validation</label>
+          <label>Apelido:</label>
           <div class="mws-form-item large">
-            <input type="text" name="ageField" class="mws-textinput required digits" />
+            <input type="text" name="apelido" class="mws-textinput required" />
           </div>
         </div>
         <div class="mws-form-row">
-          <label>Select Box Validation</label>
+          <label>Email:</label>
           <div class="mws-form-item large">
-            <select class="required" name="selectBox">
-              <option></option>
-              <option>Option 1</option>
-              <option>Option 3</option>
-              <option>Option 4</option>
-              <option>Option 5</option>
-            </select>
+            <input type="text" name="email" class="mws-textinput required email" />
           </div>
         </div>
       </div>
+      <div class="mws-form-row">
+        <label>Foto:</label>
+        <div class="mws-form-item large">
+          <input type="file" name="foto" class="mws-textinput" />
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Cargo:</label>
+        <div class="mws-form-item small">
+          <select name="cargo" class="required">
+            <option value="0">Usuario Normal</option>
+            <option value="1">Locutor</option>
+            <option value="2">DJ</option>
+            <option value="3">Administrador</option>
+          </select>
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Data de Nascimento:</label>
+        <div class="mws-form-item large">
+          <input type="text" name="datanascimento" placeholder="Formato(00/00/0000)" class="mws-textinput required date" />
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Telefone:</label>
+        <div class="mws-form-item small">
+          <input type="text" name="telefone" placeholder="Somente numeros" class="mws-textinput digits" />
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Twitter:</label>
+        <div class="mws-form-item large">
+          <input type="text" name="twitter" placeholder="Ex:http://twitter.com/meunome" class="mws-textinput url" />
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Skype:</label>
+        <div class="mws-form-item small">
+          <input type="text" name="skype" class="mws-textinput" />
+        </div>
+      </div>
+      <div class="mws-form-row">
+        <label>Facebook:</label>
+        <div class="mws-form-item large">
+          <input type="text" name="facebook" placeholder="Ex:http://facebook.com/meunome" class="mws-textinput url" />
+        </div>
+      </div>        
+
       <div class="mws-button-row">
-        <input type="submit" class="mws-button red" />
+        <input type="submit" value="Cadastrar" name="submit" class="mws-button red" />
       </div>
     </form>
-
-  </div>
+  </div> 
 </div>
